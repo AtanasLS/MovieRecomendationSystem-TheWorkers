b3ab13675383ad8c8ac989f3d95c99d8d2f37d0b
DimitarGateshki <101872373+DimitarGateshki@users.noreply.github.com>
2023-02-19 00:55:08 +0100

    Added category use
    Added category functionality for changing movie name and indicates selected category

--- src/dk/easv/presentation/controller/AppController.java
+++ src/dk/easv/presentation/controller/AppController.java
@@ -7,9 +7,13 @@ import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.paint.Color;
 
+import javax.swing.text.html.ImageView;
+import java.awt.*;
 import java.net.URL;
 import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class AppController implements Initializable {
@@ -23,6 +27,17 @@ public class AppController implements Initializable {
     //footer's label
     @FXML
     private Label footer;
+
+    @FXML
+    private Label cat1,cat2,cat3,cat4,cat5,cat6;
+
+    @FXML
+    private ImageView catMov1,catMov2,catMov3,catMov4,catMov5;
+
+
+    private List<Label> categories =new ArrayList<>();
+
+    private List<Movie> topThreeMovies;
 
 
 
@@ -46,7 +61,7 @@ public class AppController implements Initializable {
 
     public void setModel(AppModel model) {
         model.loadData(model.getObsLoggedInUser());
-       List<Movie>topThreeMovies= model.getObsTopMovieNotSeen();
+       topThreeMovies= model.getObsTopMovieNotSeen();
        topThreeMovies.sort(Comparator.comparing(Movie::getAverageRating));
 
        topOneMovTitle.setText(topThreeMovies.get(0).getTitle().toString());
@@ -60,14 +75,94 @@ public class AppController implements Initializable {
         topOneMovTitle4.setText(topThreeMovies.get((int)(Math.random()*topThreeMovies.size())+0).getTitle());
         topOneMovTitle5.setText(topThreeMovies.get((int)(Math.random()*topThreeMovies.size())+0).getTitle());
 
+
+        this.setLabels();
+        this.categories.add(cat1);
+        this.categories.add(cat2);
+        this.categories.add(cat3);
+        this.categories.add(cat4);
+        this.categories.add(cat5);
+        this.categories.add(cat6);
+
+
+    }
+
+    @FXML
+    private void setAction(){
+        this.setColour(0);
+        this.setLabels();
+        setImages();
+
+    }
+
+    @FXML
+    private void setDrama(){
+        this.setColour(1);
+        this.setLabels();
+        setImages();
+
+    }
+
+    @FXML
+    private void setComedy(){
+        this.setColour(2);
+        this.setLabels();
+        setImages();
+
+    }
+
+    @FXML
+    private void setHorror(){
+        this.setColour(3);
+        this.setLabels();
+        setImages();
+
+    }
+
+    @FXML
+    private void setAnimation(){
+        this.setColour(4);
+        this.setLabels();
+        setImages();
+
+    }
+
+    @FXML
+    private void setRomance(){
+        this.setColour(5);
+        this.setLabels();
+        setImages();
+
+    }
+
+
+
+    private void setImages(){
+        
+
+    }
+
+
+
+    private void setColour(int i){
+        for (int j = 0; j < 6; j++) {
+
+            if (j==i){
+                this.categories.get(j).setTextFill(Color.web("#C69749"));
+
+            }else {
+                this.categories.get(j).setTextFill(Color.web("#fff"));
+            }
+        }
+    }
+
+    private void setLabels(){
+
         catMovTitle1.setText(topThreeMovies.get((int)(Math.random()*topThreeMovies.size())+0).getTitle());
         catMovTitle2.setText(topThreeMovies.get((int)(Math.random()*topThreeMovies.size())+0).getTitle());
         catMovTitle3.setText(topThreeMovies.get((int)(Math.random()*topThreeMovies.size())+0).getTitle());
         catMovTitle4.setText(topThreeMovies.get((int)(Math.random()*topThreeMovies.size())+0).getTitle());
         catMovTitle5.setText(topThreeMovies.get((int)(Math.random()*topThreeMovies.size())+0).getTitle());
-
-
-
 
     }
 }
